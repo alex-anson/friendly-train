04b7ffa13b371a2784ff7c5f7987804996bb84f7
Alex Anson <1alex.anson@gmail.com>
2022-11-04 16:22:46 -0400

    first (& likely only :laughing:) commit :sparkles:

---
+++ README.md
@@ -0,0 +1,7 @@
+# Express API Example
+
+Not much to see here... & no error handling or validation to speak of.
+
+- **Start the server with `node index.js`**
+
+- vs-code extension for `example.rest` is REST Client.
---
+++ index.js
@@ -0,0 +1,49 @@
+// @ts-check
+const express = require("express");
+const app = express();
+
+// Middleware
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+const PORT = 3000;
+
+app.get("/", (_, res) => res.send("Hello World"));
+
+const sendHTML = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fox is Best</title>
+</head>
+<body>
+    <div style="font-size: 96px;">🦊 🔥 🦊 🔥 🦊 🔥 🦊</div>
+</body>
+</html>
+`;
+
+app.get("/fox", (req, res) => {
+  const { name } = req.query;
+  if (typeof name !== "string") {
+    return res.status(200).send(sendHTML);
+  }
+  res.send(`hey there, ${name} 🦊`);
+});
+
+app.get("/fox/:id", (req, res) => {
+  const { id } = req.params;
+  res.send(`user id: ${id} 🦊`);
+});
+
+app.post("/fox", (req, res) => {
+  const postData = req.body;
+  const { note, date } = postData;
+  res.send(`YOU HAVE A NEW NOTE: "${note}" \nSent: ${date}`);
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
+});
